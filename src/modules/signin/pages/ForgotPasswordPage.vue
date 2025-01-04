6bdeddfcd6f7b4c6249f5df4d23e10890fb21c62
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-01-03 23:53:45 -0300

    feat: personalizando telas

--- src/modules/signin/pages/ForgotPasswordPage.vue
+++ src/modules/signin/pages/ForgotPasswordPage.vue
@@ -1,5 +1,34 @@
 <template>
   <auth-wrapper>
+    <template v-slot:banner>
+      <div
+        class="tw-flex tw-flex-col tw-h-full tw-gap-4 tw-justify-center tw-items-center"
+      >
+        <q-intersection
+          tag="span"
+          class="sm:tw-w-[350px] md:tw-w-[400px]"
+          once
+          transition="jump-up"
+          transition-duration="3000"
+        >
+          <div class="tw-flex tw-w-full tw-items-center tw-justify-center">
+            <div class="sm:tw-w-[250px] md:tw-w-full tw-flex tw-justify-center">
+              <img src="~/assets/banner-forgot-password.svg" loading="lazy" />
+            </div>
+          </div>
+
+          <div class="tw-text-white tw-text-center tw-w-full tw-mt-4">
+            <b class="tw-text-[24px]">Esqueceu sua senha?</b>
+            <div>
+              Insira o e-mail associado à sua conta para receber um link de
+              redefinição. Caso não encontre o e-mail na caixa de entrada,
+              verifique o spam ou lixo eletrônico. Estamos aqui para garantir
+              sua segurança!
+            </div>
+          </div>
+        </q-intersection>
+      </div>
+    </template>
     <template v-slot:content>
       <div class="tw-flex tw-h-full tw-items-center tw-justify-center">
         <form-forgot-password />
