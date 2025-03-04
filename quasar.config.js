42f30bbc5523d727218a7cfbff9e63c3bf5512bd
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-03-03 23:14:49 -0300

    feat: ajuste build

--- quasar.config.js
+++ quasar.config.js
@@ -101,6 +101,30 @@ module.exports = configure(function (/* ctx */) {
               //   const parts = id.split('node_modules/')[1].split('/');
               //   return `vendor-${parts[0]}`;
               // }
+
+              if (id.includes('node_modules')) {
+                const vendorMap = new Map([
+                  ['jspdf', 'vendor-jspdf'],
+                  ['html2canvas', 'vendor-html2canvas'],
+                  ['moment', 'vendor-moment'],
+                  ['jspdf-autotable', 'vendor-jspdf-autotable'],
+                  ['runtime-core', 'vendor-runtime-core'],
+                  ['vue', 'vendor-vue'],
+                  ['vue-router', 'vendor-vue-router'],
+                  ['axios', 'vendor-axios'],
+                  ['crypto-js', 'vendor-crypto-js'],
+                  ['canvg', 'vendor-canvg'],
+                ]);
+
+                const vendor = [...vendorMap.keys()].find((lib) =>
+                  id.includes(lib)
+                );
+                if (vendor) return vendorMap.get(vendor);
+
+                const parts = id.split('node_modules/')[1].split('/');
+                return `vendor-${parts[0]}`;
+              }
+
               if (id.includes('/src/shared/components/')) {
                 return `component-${generateRandomName()}`;
               }
