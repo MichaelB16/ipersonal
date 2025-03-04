3336345a47874c11c0dd01bfcb71ef83ba1274dc
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-03-03 23:18:37 -0300

    feat: ajuste build

--- quasar.config.js
+++ quasar.config.js
@@ -11,9 +11,9 @@
 const { configure } = require('quasar/wrappers');
 const env = require('dotenv');
 
-const generateRandomName = () => {
-  return Math.random().toString(36).substring(2, 15);
-};
+// const generateRandomName = () => {
+//   return Math.random().toString(36).substring(2, 15);
+// };
 
 module.exports = configure(function (/* ctx */) {
   return {
